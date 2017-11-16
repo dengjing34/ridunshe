1895df8b6aff7cd91acdd132fbbafb21d320538a
dengjing <jingd3@jumei.com>
2017-11-17 03:32:53 +0800

    add cache and upload keep file;config sample files

---
+++ config/imagesize.sample.php
@@ -0,0 +1,21 @@
+<?php
+//dengjing34@vip.qq.com
+return array(
+    'small' => array(
+        'width' => '32',
+        'height' => '23',
+    ),
+    'middle' => array(
+        'width' => '64',
+        'height' => '46',
+    ),    
+    'big' => array(
+        'width' => '96',
+        'height' => '70',
+    ),
+    'bigger' => array(
+        'width' => '210',
+        'height' => '152',
+    ),
+);
+?>
---
+++ config/nav.sample.php
@@ -0,0 +1,51 @@
+<?php
+//dengjing34@vip.qq.com
+return array(
+    'home' => array(
+        'text' => '首页',
+        'url' => 'homepage',
+    ),
+    'works' => array(
+        'text' => '作品',
+        'url' => 'works',
+    ),    
+    'about' => array(
+        'text' => '我们',
+        'url' => 'about',
+        'pages' => true,//单独页面
+    ),
+    'news' => array(
+        'text' => '新闻',
+        'url' => 'news',
+    ),    
+//    'staff' => array(
+//        'text' => '团队',
+//        'url' => 'staff',
+//        'pages' => true,
+//    ),
+//    'voice' => array(
+//        'text' => '声音',
+//        'url' => 'voice',
+//    ),
+//    'blog' => array(
+//        'text' => '微博',
+//        'url' => 'blog',
+//        'pages' => true,
+//    ),
+    'contact' => array(
+        'url' => 'contact',
+        'text' => '联系',
+        'pages' => true,
+    ),
+    'services' => array(
+        'url' => 'services',
+        'text' => '服务',
+        'pages' => true,
+    ),
+    'process' => array(
+        'url' => 'process',
+        'text' => '流程',
+        'pages' => true,
+    )
+);
+?>
