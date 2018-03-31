4bff6dfcdbac0f3cce422697769985141b78d896
dengjing <jingd3@jumei.com>
2018-03-31 11:54:39 +0800

    update

--- view/chibi/works/workList.php
+++ view/chibi/works/workList.php
@@ -26,6 +26,10 @@ $emptyMsg = $dataList = '';
             <td>名称</td>
             <td>副标题</td>
             <td>分类</td>
+            <td>首页滚动图片</td>
+            <td>首页滚动图片排序</td>
+            <td>首页底部图片</td>
+            <td>首页底部图片排序</td>
             <td>封面</td>
             <td>排序</td>
             <td>创建时间</td>
@@ -37,12 +41,18 @@ if (empty($objs)) {
 } else {
     foreach ($objs as $obj) {
         $pic = (bool)$obj->cover ? "<a href=\"" .  Url::fileUrl($obj->cover) . "\" target=\"_blank\" title=\"点击查看原图\"><img src=\"" . Url::smallImage($obj->cover) . "\" alt=\"点击查看原图\" /></a>" : null;
+        $banner_pic = (bool)$obj->banner_pic ? "<a href=\"" .  Url::fileUrl($obj->banner_pic) . "\" target=\"_blank\" title=\"点击查看原图\"><img src=\"" . Url::smallImage($obj->banner_pic) . "\" alt=\"点击查看原图\" /></a>" : null;
+        $home_pic = (bool)$obj->home_pic ? "<a href=\"" .  Url::fileUrl($obj->home_pic) . "\" target=\"_blank\" title=\"点击查看原图\"><img src=\"" . Url::smallImage($obj->home_pic) . "\" alt=\"点击查看原图\" /></a>" : null;
         $dataList .= "<tr>";
         $dataList .= "<td><input type=\"checkbox\" name=\"id[]\" value=\"{$obj->id}\" />{$obj->id}</td>";
         $dataList .= "<td><a class=\"button button-orange\" href=\"" . Url::siteUrl("chibi/{$controller}/{$controller}Modify?id={$obj->id}")  . "\"><span class=\"pencil\"></span>编辑</a></td>";
         $dataList .= "<td><a href=\"" . Url::siteUrl("chibi/{$controller}/{$controller}Modify?id={$obj->id}") . "\">{$obj->title}</a></td>";
         $dataList .= "<td>{$obj->sub_title}</td>";
         $dataList .= "<td>{$obj->category_zh}</td>";
+        $dataList .= "<td>{$banner_pic}</td>";
+        $dataList .= "<td>{$obj->banner_sort}</td>";
+        $dataList .= "<td>{$home_pic}</td>";
+        $dataList .= "<td>{$obj->home_sort}</td>";
         $dataList .= "<td>{$pic}</td>";
         $dataList .= "<td>{$obj->sort}</td>";
         $dataList .= "<td>" . date('Y-m-d H:i:s', $obj->create_time) ."</td>";
